47fe5da91b6b86caae131b729eb3f9d442ac4ab4
Mac <mac@Macs-MBP.lan>
2021-01-15 16:55:19 +0000

    1st commit

---
+++ script.js
@@ -0,0 +1,210 @@
+window.addEventListener('scroll', function(){
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+}); 
+
+function toggleMenu(){
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+// function toggleMenu(x) {
+//     x.classList.toggle("change");
+//   }
+
+
+// Form 1  DELETE OR KEEP AFTER CONFIRMATION ------------
+// $(function()
+// {
+//     function after_form_submitted(data)
+//     {
+//         if(data.result == 'success')
+//         {
+//             $('form#reused_form').hide();
+//             $('#success_message').show();
+//             $('#error_message').hide();
+//         }
+//         else
+//         {
+//             $('#error_message').append('<ul></ul>');
+
+//             jQuery.each(data.errors,function(key,val)
+//             {
+//                 $('#error_message ul').append('<li>'+key+':'+val+'</li>');
+//             });
+//             $('#success_message').hide();
+//             $('#error_message').show();
+
+//             //reverse the response on the button
+//             $('button[type="button"]', $form).each(function()
+//             {
+//                 $btn = $(this);
+//                 label = $btn.prop('orig_label');
+//                 if(label)
+//                 {
+//                     $btn.prop('type','submit' );
+//                     $btn.text(label);
+//                     $btn.prop('orig_label','');
+//                 }
+//             });
+
+//         }//else
+//     }
+
+// 	$('#reused_form').submit(function(e)
+//       {
+//         e.preventDefault();
+
+//         $form = $(this);
+//         //show some response on the button
+//         $('button[type="submit"]', $form).each(function()
+//         {
+//             $btn = $(this);
+//             $btn.prop('type','button' );
+//             $btn.prop('orig_label',$btn.text());
+//             $btn.text('Sending ...');
+//         });
+
+
+//                     $.ajax({
+//                 type: "POST",
+//                 url: 'handler.php',
+//                 data: $form.serialize(),
+//                 success: after_form_submitted,
+//                 dataType: 'json'
+//             });
+
+//       });
+// });
+
+// FORMULÁRIO 2 teste  
+
+(function ($) {
+    "use strict";
+
+    
+    /*==================================================================
+    [ Validate ]*/
+    var name = $('.validate-input input[name="name"]');
+    var email = $('.validate-input input[name="email"]');
+    var subject = $('.validate-input input[name="subject"]');
+    var message = $('.validate-input textarea[name="message"]');
+
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        if($(name).val().trim() == ''){
+            showValidate(name);
+            check=false;
+        }
+
+        if($(subject).val().trim() == ''){
+            showValidate(subject);
+            check=false;
+        }
+
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check=false;
+        }
+
+        if($(message).val().trim() == ''){
+            showValidate(message);
+            check=false;
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input1').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+       });
+    });
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+})(jQuery);
+
+
+
+
+
+
+(function ($) {
+    "use strict";
+
+    
+    /*==================================================================
+    [ Validate ]*/
+    var name = $('.validate-input input[name="name"]');
+    var email = $('.validate-input input[name="email"]');
+    var subject = $('.validate-input input[name="subject"]');
+    var message = $('.validate-input textarea[name="message"]');
+
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        if($(name).val().trim() == ''){
+            showValidate(name);
+            check=false;
+        }
+
+        if($(subject).val().trim() == ''){
+            showValidate(subject);
+            check=false;
+        }
+
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check=false;
+        }
+
+        if($(message).val().trim() == ''){
+            showValidate(message);
+            check=false;
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input1').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+       });
+    });
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
