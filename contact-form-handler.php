1e90840f58c855ccdb5ebb73df2324882e114519
NelsonAires <nelson-dar@hotmail.com>
2021-01-16 18:53:47 +0000

    php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -3,7 +3,7 @@
     $visitor_email = $_POST['email'];
     $message = $_POST['message'];
 
-    $email_from = 'nelsondar@gmail.com';
+    $email_from = 'nelson-dar@hotmail.com';
     $email_subject = "Nova Mensagem";
 
     $email_body = "User Name: $name.\n".
